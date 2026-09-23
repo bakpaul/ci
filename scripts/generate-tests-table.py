691968d7b6642c9b48bb9610cefdb4e05c581d90
Paul Baksic <paul.baksic@outlook.fr>
2026-09-23 17:14:52 +0200

    fix test table summary

--- scripts/generate-tests-table.py
+++ scripts/generate-tests-table.py
@@ -2,6 +2,7 @@ from prettytable import PrettyTable
 from prettytable.colortable import ColorTable, Themes
 import os
 import sys
+from pathlib import Path
 
 
 
@@ -12,19 +13,19 @@ if __name__ == "__main__":
         exit(1)
 
     noraml_suf="\033[0m"
-    bold_pre="\x1b[1m"
-    ok_pre="\x1b[0;32m"
-    fault_pre="\x1b[1;31m"
-    not_ran_pre="\x1b[2m"
+    bold_pre="\033[1m"
+    ok_pre="\033[32m"
+    fault_pre="\033[31m"
+    not_ran_pre="\033[36m"
 
     tests = ['unit', 'scene', 'regression']
-    table_rows = ["ran", "failed", "errors", "duration (s)"]
+    table_rows = ["ran", "failed", "crashed", "duration (s)"]
 
-    logs_dir=sys.argv[1]
+    logs_dir=Path(sys.argv[1])
 
     #Determine which test has been ran
-    results_files=[os.path.abspath(os.path.join(logs_dir, test_name + "-tests/" + test_name + "-tests_results.txt")) for test_name in tests ]
-    test_ran=[os.path.isfile(res_file) for res_file in results_files ]
+    results_files=[(logs_dir / test_name / "summary.txt").absolute() for test_name in tests ]
+    test_ran=[res_file.is_file() for res_file in results_files ]
 
     #Fill table given files content. Crashes and failures are associated.
     table_content = [[0 for j in table_rows] for i in tests]
@@ -32,15 +33,17 @@ if __name__ == "__main__":
         if test_ran[i]:
             with open(results_files[i]) as file:
                 for line in file:
-                    res_type = line.strip().split('_')[1].split('=')[0]
+                    print(line)
+
+                    res_type = line.strip().split('=')[0]
                     value = int(line.strip().split('=')[1].split('.')[0])
-                    if res_type=="total":
+                    if res_type=="test_total":
                         table_content[i][0] += value
-                    elif res_type=="disabled":
+                    elif res_type=="disabled_tests":
                         table_content[i][0] -= value
-                    elif res_type=="crashes" or res_type=="failures" :
+                    elif res_type=="failures" :
                         table_content[i][1] += value
-                    elif res_type=="errors":
+                    elif res_type=="crashes":
                         table_content[i][2] = value
                     elif res_type=="duration":
                         table_content[i][3] = value
@@ -68,11 +71,3 @@ if __name__ == "__main__":
 
     #Finaly print it
     print(res_table)
-
-
-
-
-
-
-
-
